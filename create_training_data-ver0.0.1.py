b9bc15c8e2a6c65bc3c6f0ecebfc43d26bfc5ed7
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-06-02 21:48:23 +0900

    ##

--- create_training_data-ver0.0.1.py
+++ create_training_data-ver0.0.1.py
@@ -1,7 +1,14 @@
 import json
 import copy
 import pathlib
+import os
+from datetime import datetime
+import sys
 
+import glob
+
+from gensim.models.doc2vec import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
 
 """
     - 定数周り
@@ -11,22 +18,89 @@ SELF_MADE_DATASETS_GOLD_PATH = "./self-made-datasets/gold/"
 SELF_MADE_DATASETS_GITHUB_PATH = "./self-made-datasets/github/"
 SELF_MADE_DATASETS_ORIGIN_PATH = "./self-made-datasets/origin/"
 
+"""
+    - goldルールのみのファイルパスを取得するためのパス
+"""
+PATCH_GLOB_SELF_MADE_DATASETS_GOLD_PATH = "./self-made-datasets/gold/**"
+
+
+class D2V(object):
+    @staticmethod
+    def do(training_data, min_count=100, dm=1, window=5, name="default", types="github"):
+        """
+            - モデルの作成
+        """
+        current_time = datetime.now()
+        current_time_str = current_time.strftime('%Y-%m-%d-%H-%M-%S')
+        documents = [TaggedDocument(words=token, tags=[tag_name]) for tag_name, token in training_data.items()]
+        pass
+        model = Doc2Vec(
+            documents=documents,
+            min_count=min_count,
+            dm=dm,
+            window=window
+        )
+        if dm==1:
+            model_name = "./self-made-model/{}/dmpv/{}_{}.model".format(types, name, current_time)
+        else:
+            model_name = "./self-made-model/{}/dbow/{}_{}.model".format(types, name, current_time)
+        
+        model.save(model_name)
+    
+    @staticmethod
+    def epoch(training_data, min_count=100, dm=1, window=5, epochs=20, name="default", types="github"):
+        """
+            - epochでの実装, モデルの作成
+        """
+        current_time = datetime.now()
+        current_time_str = current_time.strftime('%Y-%m-%d-%H-%M-%S')
+        documents = [TaggedDocument(words=token, tags=[tag_name]) for tag_name, token in training_data.items()]
+        pass
+        model = Doc2Vec(
+            documents=documents,
+            min_count=min_count,
+            dm=dm,
+            window=window
+        )
+        if dm==1:
+            model_name = "./self-made-model/{}/dmpv/epoch-{}_{}_{}.model".format(types, epochs, name, current_time)
+        else:
+            model_name = "./self-made-model/{}/dbow/epoch-{}_{}_{}.model".format(types, epochs, name, current_time)
+        
+        alpha = 0.025
+        alpha_delta = 0.001
+
+        los_vals = []
+        for epoch in range(epochs):
+            print("Epoch: {}".format(epoch + 1))
+            model.alpha, model.min_alpha = alpha, alpha
+            model.train(documents, total_examples=model.corpus_count, epochs=model.iter)
+            alpha -= alpha_delta
+            los_vals.append(model.get_latest_training_loss())
+        
+        model.save(model_name)
+            
+
 class TR(object):
     pass
     @staticmethod
-    def do(ast_obj, tr_data=dict()):
+    def do(ast_obj):
         children = ast_obj.children
         if not children:
-            return
+            return None
+        trs = dict()
         for hg, child in enumerate(children):
             tokens = Recursive.do(child)
             for wd, token in enumerate(tokens):
-                print(hg, wd, token)
                 tr = {
                     "{}:{}:{}".format(ast_obj.file_sha, hg, wd) : token
                 }
-                tr_data.update(tr)
-        return tr_data
+                trs.update(tr)
+        return trs
+                 
+    @staticmethod
+    def does():
+        pass
 
 
 class Recursive(object):
@@ -58,14 +132,32 @@ class MetaData(object):
         with open(METADATA_SHA_PATH, mode="r") as f:
             data = json.load(f)
         return data["file_sha"]
+    
+    @staticmethod
+    def patch():
+        file_shas = list()
+        origins = [origin for origin in glob.glob(PATCH_GLOB_SELF_MADE_DATASETS_GOLD_PATH, recursive=True)]
+        for origin in origins:
+            base_name = os.path.basename(origin)
+            file_sha = base_name.replace(".json", "")
+            file_shas.append(file_sha)
+        return file_shas
 
 class BaseAST(object):
     def __init__(self, file_sha):
+        def exists() -> bool:
+            return os.path.exists(file_path)
         file_path = "{}{}.json".format(SELF_MADE_DATASETS_GITHUB_PATH, file_sha); file_path = str(pathlib.Path(file_path).resolve())
-        with open(file_path, mode="r") as f:
-            obj = json.load(f)
+        if exists():
+            with open(file_path, mode="r") as f:
+                obj = json.load(f)
+        else:
+            obj = dict()
         self._children = obj
         self._file_sha = file_sha
+    
+    
+
 
     @property
     def children(self):
@@ -84,16 +176,48 @@ class BaseAST(object):
         return self._file_sha
     
 
-def main():
-    TEST_INDEX = 18
+def create_github_training_data() -> dict:
     file_sha = MetaData.get_sha()
     tr_data = dict()
     for sha in file_sha:
         ast_obj = BaseAST(sha)
-        tr_data = TR.do(ast_obj, tr_data)
+        trs = TR.do(ast_obj)
+        if trs:
+            tr_data.update(trs)
+    return tr_data
+
+def check_num_of_gold_word():
+    tr_data = dict()
+    file_sha = MetaData.patch()
+    for sha in file_sha:
+        ast_obj = BaseAST(sha)
+        trs = TR.do(ast_obj)
+        if trs:
+            tr_data.update(trs)
     
-    for key, value in tr_data.items():
-        print(key, value)
+    cnt = 0
+    for tr in tr_data.values():
+        cnt += len(tr)
+
+    print(cnt)
+
+def create_gold_training_data() -> dict:
+    tr_data = dict()
+    file_sha = MetaData.patch()
+    for sha in file_sha:
+        ast_obj = BaseAST(sha)
+        trs = TR.do(ast_obj)
+        if trs:
+            tr_data.update(trs)
+
+    return tr_data
+
+def main():
+    tr_data = create_github_training_data()
+    # file_sha = MetaData.get_sha()
+    D2V.do(tr_data, min_count=100, dm=0, window=5, name="default", types="github")
+    
+    
     
 
 
